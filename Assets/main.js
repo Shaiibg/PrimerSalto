d9f0eab0902b01df622f657d1d6cf13f8fe86c22
Oscar18900 <147523781+Oscar18900@users.noreply.github.com>
2024-11-27 17:10:34 -0600

    Update main.js

--- Assets/main.js
+++ Assets/main.js
@@ -1,3 +1,25 @@
-function start () {
-    alert("Hola");
+var modal = document.getElementById("myModal"); // Reemplaza "myModal" con el ID de tu modal
+var img = document.getElementById("myImg"); // Reemplaza "myImg" con el ID de tu imagen
+var modalImg = document.getElementById("img01"); // Reemplaza "img01" con el ID de la imagen dentro del modal
+var captionText = document.getElementById("caption"); // Reemplaza "caption" con el ID del texto del pie de foto (opcional)
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
 }
+
+var span = document.getElementsByClassName("cerrar")[0];   
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+function abrirModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "block";
+  }
+  
+  function cerrarModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "none";
+  }
